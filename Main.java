f446b43f5506bea177a0241e73bb968fab9e8046
Abir Ghosh <abir20021116@gmail.com>
2025-06-28 18:28:36 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import All_JAVA_File.SplashScreen;
+
+
+public class Main{
+	
+	public static void main(String []ars)
+	{
+		//new SignEmail();
+		SplashScreen splash = new SplashScreen(2000);
+	}  
+}   
---
+++ Main_For_Recharge_Money.java
@@ -0,0 +1,11 @@
+import All_JAVA_File.Balance;
+
+public class Main_For_Recharge_Money{
+	
+	public static void main(String []ars)
+	{
+	
+	new Balance();
+
+	}  
+}   
